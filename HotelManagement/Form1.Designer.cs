e33c9253980734eef542485648fd44c7372230f6
KyryloMuzychka <kyryll2016@gmail.com>
2025-04-13 20:06:23 +0300

    feat: auth design

---
+++ HotelManagement/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace HotelManagement
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.btnExit = new Guna.UI2.WinForms.Guna2CircleButton();
+			this.label3 = new System.Windows.Forms.Label();
+			this.labelError = new System.Windows.Forms.Label();
+			this.btnLogin = new Guna.UI2.WinForms.Guna2Button();
+			this.txtPassword = new Guna.UI2.WinForms.Guna2TextBox();
+			this.txtUsername = new Guna.UI2.WinForms.Guna2TextBox();
+			this.guna2CirclePictureBox2 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+			this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.btnExit);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.labelError);
+			this.panel1.Controls.Add(this.btnLogin);
+			this.panel1.Controls.Add(this.txtPassword);
+			this.panel1.Controls.Add(this.txtUsername);
+			this.panel1.Controls.Add(this.guna2CirclePictureBox2);
+			this.panel1.Controls.Add(this.guna2CirclePictureBox1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(-19, -19);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(716, 373);
+			this.panel1.TabIndex = 0;
+			// 
+			// btnExit
+			// 
+			this.btnExit.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+			this.btnExit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+			this.btnExit.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+			this.btnExit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+			this.btnExit.FillColor = System.Drawing.Color.White;
+			this.btnExit.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.btnExit.ForeColor = System.Drawing.Color.White;
+			this.btnExit.Image = global::HotelManagement.Properties.Resources.cancel_50px;
+			this.btnExit.ImageSize = new System.Drawing.Size(30, 30);
+			this.btnExit.Location = new System.Drawing.Point(31, 31);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+			this.btnExit.Size = new System.Drawing.Size(35, 35);
+			this.btnExit.TabIndex = 8;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.Gray;
+			this.label3.Location = new System.Drawing.Point(72, 345);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(545, 18);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "* You will accept our terms and conditions. When you login using valid credential" +
+    "s.";
+			// 
+			// labelError
+			// 
+			this.labelError.AutoSize = true;
+			this.labelError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelError.ForeColor = System.Drawing.Color.Red;
+			this.labelError.Location = new System.Drawing.Point(415, 309);
+			this.labelError.Name = "labelError";
+			this.labelError.Size = new System.Drawing.Size(218, 18);
+			this.labelError.TabIndex = 6;
+			this.labelError.Text = "Wrong Username or Password!";
+			this.labelError.Visible = false;
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.BorderRadius = 18;
+			this.btnLogin.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+			this.btnLogin.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+			this.btnLogin.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+			this.btnLogin.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+			this.btnLogin.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.btnLogin.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLogin.ForeColor = System.Drawing.Color.White;
+			this.btnLogin.Location = new System.Drawing.Point(412, 254);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(240, 45);
+			this.btnLogin.TabIndex = 5;
+			this.btnLogin.Text = "Login";
+			this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+			// 
+			// txtPassword
+			// 
+			this.txtPassword.BorderRadius = 18;
+			this.txtPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.txtPassword.DefaultText = "";
+			this.txtPassword.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+			this.txtPassword.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+			this.txtPassword.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.txtPassword.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.txtPassword.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.txtPassword.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+			this.txtPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.txtPassword.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.txtPassword.IconLeft = global::HotelManagement.Properties.Resources.lock_25px;
+			this.txtPassword.Location = new System.Drawing.Point(412, 177);
+			this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.txtPassword.Name = "txtPassword";
+			this.txtPassword.PasswordChar = '*';
+			this.txtPassword.PlaceholderText = "Password";
+			this.txtPassword.SelectedText = "";
+			this.txtPassword.Size = new System.Drawing.Size(240, 49);
+			this.txtPassword.TabIndex = 4;
+			this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// txtUsername
+			// 
+			this.txtUsername.BorderRadius = 18;
+			this.txtUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.txtUsername.DefaultText = "";
+			this.txtUsername.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+			this.txtUsername.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+			this.txtUsername.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.txtUsername.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.txtUsername.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.txtUsername.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.txtUsername.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.txtUsername.IconLeft = global::HotelManagement.Properties.Resources.user_25px;
+			this.txtUsername.Location = new System.Drawing.Point(412, 120);
+			this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.txtUsername.Name = "txtUsername";
+			this.txtUsername.PlaceholderText = "Username";
+			this.txtUsername.SelectedText = "";
+			this.txtUsername.Size = new System.Drawing.Size(240, 49);
+			this.txtUsername.TabIndex = 3;
+			this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// guna2CirclePictureBox2
+			// 
+			this.guna2CirclePictureBox2.BackColor = System.Drawing.Color.Transparent;
+			this.guna2CirclePictureBox2.Image = global::HotelManagement.Properties.Resources.circular_loading;
+			this.guna2CirclePictureBox2.ImageRotate = 0F;
+			this.guna2CirclePictureBox2.Location = new System.Drawing.Point(25, 40);
+			this.guna2CirclePictureBox2.Name = "guna2CirclePictureBox2";
+			this.guna2CirclePictureBox2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+			this.guna2CirclePictureBox2.Size = new System.Drawing.Size(319, 297);
+			this.guna2CirclePictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.guna2CirclePictureBox2.TabIndex = 2;
+			this.guna2CirclePictureBox2.TabStop = false;
+			this.guna2CirclePictureBox2.UseTransparentBackground = true;
+			// 
+			// guna2CirclePictureBox1
+			// 
+			this.guna2CirclePictureBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.guna2CirclePictureBox1.Image = global::HotelManagement.Properties.Resources.login;
+			this.guna2CirclePictureBox1.ImageRotate = 0F;
+			this.guna2CirclePictureBox1.Location = new System.Drawing.Point(-27, 47);
+			this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+			this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+			this.guna2CirclePictureBox1.Size = new System.Drawing.Size(424, 295);
+			this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.guna2CirclePictureBox1.TabIndex = 1;
+			this.guna2CirclePictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Comic Sans MS", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(425, 47);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(210, 51);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "User Login";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.BackgroundImage = global::HotelManagement.Properties.Resources.login_image;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(1018, 598);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+		private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox2;
+		private Guna.UI2.WinForms.Guna2TextBox txtUsername;
+		private Guna.UI2.WinForms.Guna2TextBox txtPassword;
+		private Guna.UI2.WinForms.Guna2Button btnLogin;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label labelError;
+		private Guna.UI2.WinForms.Guna2CircleButton btnExit;
+	}
+}
+
